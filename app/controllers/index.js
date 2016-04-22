f4d451c4c891da6384fc3af793d993a1ccad43cc
Jeremy Green <jeremy@octolabs.com>
2016-04-21 23:37:37 -0500

    Remove some logging statements

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     chooseMyShow(showId){
-      console.log('starting chooseMyShow');
       this.transitionToRoute('my-show', showId);
     }
   }
--- app/routes/my-show.js
+++ app/routes/my-show.js
@@ -6,10 +6,7 @@ export default Ember.Route.extend({
   myShows: inject.service(),
   model({eventId}) {
     let events = this.get('myShows.shows');
-    console.log('events = ', events);
     var event = events.findBy('id', eventId);
-    console.log('eventId = ', eventId);
-    console.log('event = ', event);
     return event;
   }
 });
